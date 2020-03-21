8e4e5e55156b47c8e9ae2fb233cc82c4527ca433
Sebastian Sauer <sauer.sebastian@gmail.com>
2020-03-21 09:27:23 +0100

    Seeds for patients

--- receptionist/db/seeds.rb
+++ receptionist/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Generate 50 patients
+for i in 1..25 do
+    puts i
+    Patient.create( :birthday => DateTime.strptime("09/"+i.to_s+"/2009 8:00", "%m/%d/%Y %H:%M"), :timeToDoc => i * 2, :phone => "015111111" + i.to_s, :email => "horst" + i.to_s + "@gmail.com")
+end
