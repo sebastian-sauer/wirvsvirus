6569cff0ca557f8cfe616b8d9fe226ac80bd9583
Flavio Diez <flaviofdiez@gmail.com>
2020-03-21 10:54:03 +0100

    testing relationships

--- receptionist/app/controllers/line_admin_controller.rb
+++ receptionist/app/controllers/line_admin_controller.rb
@@ -2,5 +2,8 @@ class LineAdminController < ApplicationController
   def index
     @appointments = Appointment.all
     @patients = Patient.all
+
+    dt = DateTime.strptime("02.03.2020 8:00", "%d.%m.%Y %H:%M")
+    @times = Appointment.where(datetime: dt.all_day)
   end
 end
