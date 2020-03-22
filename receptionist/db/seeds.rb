443ae3f4d1189e0823a6f1deaaf5bfb7fca2b476
Sebastian Sauer <sauer.sebastian@gmail.com>
2020-03-22 08:29:23 +0100

    Seed data for today

--- receptionist/db/seeds.rb
+++ receptionist/db/seeds.rb
@@ -1,13 +1,14 @@
 # Generate 25 patients
 
 time = DateTime.strptime("21.03.2020 7:30", "%d.%m.%Y %H:%M")
+time = DateTime.current()
+time.change({ hour: 7, min: 30, sec: 0 })
 order = 0
 
 p = Patient.create(:name => "DER WAR SCHON DRAN ", :birthday => DateTime.strptime("09/21/2009 8:00", "%m/%d/%Y %H:%M"), :timeToDoc => 5, :phone => "015111111", :email => "horsti"+ "@gmail.com")
 p.appointments.create(:sortOrder => order, :datetime => time, :status => "done", :classification => "corona-verdacht", :remarks => "Test schon dran")
 
-
-for i in 1..5 do
+for i in 1..20 do
   time = time + 30.minutes
   order += 1
 
