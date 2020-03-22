272630e8c7078979dcc7cd58886c38154babd0c1
Sebastian Sauer <sauer.sebastian@gmail.com>
2020-03-22 11:42:17 +0100

    Add more random seeds

--- receptionist/app/models/appointment.rb
+++ receptionist/app/models/appointment.rb
@@ -6,8 +6,28 @@ class Appointment < ApplicationRecord
 
   def estimatedWaitingTime
     appointments = Appointment.where(datetime: self.datetime.all_day).where(sortOrder: 0..self.sortOrder).where.not(appointmentstatus: :done).order(:sortOrder)
-    return appointments.length() * 30
+    waitingTime = 0
+    appointments.each { |a| waitingTime+=a.estimatedTimeForTreatment }
+
+    return waitingTime
   end
+
+  # Calculates the estimated time for this treatment
+  def estimatedTimeForTreatment
+    if self.covid19?
+      return 10
+    elsif self.standard?
+      return 10
+    elsif self.covid19suspect?
+      return 30
+    elsif self.riskoflife?
+      return 10
+    else
+      # any unknown classification?
+      return 45
+    end
+  end
+
 
   def as_json(options = { })
     super((options || { }).merge({
--- receptionist/db/seeds.rb
+++ receptionist/db/seeds.rb
@@ -5,6 +5,8 @@ time = DateTime.current()
 time.change({ hour: 7, min: 30, sec: 0 })
 order = 0
 
+classifications = [Appointment.classifications[:covid19suspect], Appointment.classifications[:covid19], Appointment.classifications[:riskoflife], Appointment.classifications[:standard]]
+
 p = Patient.create(:name => "DER WAR SCHON DRAN ", :birthday => DateTime.strptime("09/21/2009 8:00", "%m/%d/%Y %H:%M"), :timeToDoc => 5, :phone => "015111111", :email => "horsti"+ "@gmail.com")
 p.appointments.create(:sortOrder => order, :datetime => time, :appointmentstatus => Appointment.appointmentstatuses[:done], :classification => Appointment.classifications[:covid19], :remarks => "Test schon dran")
 
@@ -14,5 +16,5 @@ for i in 1..20 do
 
   p = Patient.create(:name => "Horst " + i.to_s, :birthday => DateTime.strptime("09/" + i.to_s + "/2009 8:00", "%m/%d/%Y %H:%M"), :timeToDoc => i * 2, :phone => "015111111" + i.to_s, :email => "horst" + i.to_s + "@gmail.com")
 
-  p.appointments.create(:sortOrder => order, :datetime => time, :appointmentstatus => Appointment.appointmentstatuses[:newappointment], :classification => Appointment.classifications[:covid19suspect], :remarks => "Test " + i.to_s)
+  p.appointments.create(:sortOrder => order, :datetime => time, :appointmentstatus => Appointment.appointmentstatuses[:newappointment], :classification => classifications.at(i % 4), :remarks => "Test " + i.to_s)
 end
