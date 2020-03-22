5ca48739a050fd00937b3c902e6cefc7ad01f458
Flavio Diez <flaviofdiez@gmail.com>
2020-03-22 10:17:45 +0100

    header and date localization

--- receptionist/app/controllers/line_admin_controller.rb
+++ receptionist/app/controllers/line_admin_controller.rb
@@ -5,24 +5,24 @@ class LineAdminController < ApplicationController
     puts date
 
     if date.present?
-      dt = Date.strptime(date, "%Y%m%d")
+      dt = DateTime.strptime(date, "%Y%m%d")
     else
       dt = DateTime.current()
     end
 
-    @date = dt.strftime("%Y-%m-%d")
-    @nextday = dt.next_day.strftime("%Y%m%d")
-    @previousday = dt.prev_day.strftime("%Y%m%d")
+    @date = I18n.l(dt, format: :ger_with_weekday, locale: "de")
+    @nextday = dt.next_day.to_s(:year_month_day)
+    @previousday = dt.prev_day.to_s(:year_month_day)
     # to be done: check if free appointments are available for today.
     @freeAppointmentsAvailable = true
 
     @appointmentsScheduled = Appointment.where(datetime: dt.all_day).where.not(status: "done").where.not(status: "to-be-triaged").order(:sortOrder)
-    
+
     # only if there are no free appointments search for appointments to be triaged.
     # otherwise just use an empty list
     if @freeAppointmentsAvailable
       @appointmentsWaitingList = []
-    else 
+    else
       @appointmentsWaitingList = Appointment.where(datetime: dt.all_day).where.not(status: "done").where(status: "to-be-triaged").order(:sortOrder)
     end
   end
@@ -33,7 +33,7 @@ class LineAdminController < ApplicationController
     puts "clients new order is: " + newOrderedList.to_s
     # get all appointments for this day
     appointments = Appointment.where(id: newOrderedList)
-    
+
     # and recalculate the order
     for appointment in appointments do
       appointment.sortOrder = newOrderedList.index(appointment.id)
